64dac431193ea6582c9896f88d650c9871057fbd
kunal patil <patilkunnu624@gmail.com>
2025-09-20 17:14:21 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const port = 3000;
 app.use(express.json());
 app.use(clerkMiddleware())
 app.use(cors({
-    origin: ["http://localhost:5173" ,"http://localhost:5174", "https://movie-web-user-frontend.vercel.app"],
+    origin: ["http://localhost:5173" ,"http://localhost:5174", "https://movie-web-umui.vercel.app"],
     credentials: true,
 }));
 app.use(express.urlencoded({ extended: true }));
